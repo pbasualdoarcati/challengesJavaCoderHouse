8d74fc7ea58ce40e85c342a92b8dbab5546bcc58
pablo <pablo@serfe.com>
2023-08-21 19:30:54 -0300

    first commit and first challenge

---
+++ FirstChallengeFloatTypeBasualdoArcati.java
@@ -0,0 +1,30 @@
+/**
+ * The object this first challenge is find a method that returns the integer part
+ * and separated the decimal part of a float type 
+ */
+public class FirstChallengeFloatTypeBasualdoArcati {
+
+    public static void main(String[] args) {
+        System.out.println("In this challenge we will find the integer part and the decimal part of a float type");
+        float number = 3.14159265f;
+        float integerPart = (int) number;
+        float decimalPart = number - integerPart;
+        System.out.println("The float number is: " + number);
+        System.out.println("The integer part is: " + integerPart);
+        System.out.println("The decimal part is: " + decimalPart);
+        System.out.println("Do you want to enter a number and see the floating part and the integer part? ");
+        System.out.println("If you want to enter a number, please enter 1, if not, please enter 0");
+        int userInput = Integer.parseInt(System.console().readLine());
+        if (userInput == 1){
+            System.out.println("Please enter a number: ");
+            float userNumber = Float.parseFloat(System.console().readLine());
+            float userIntegerPart = (int) userNumber;
+            float decimalPartUser = userNumber - userIntegerPart;
+            System.out.println("The float number is: " + userNumber);
+            System.out.println("The integer part is: " + userIntegerPart);
+            System.out.println("The decimal part is: " + decimalPartUser);
+        }else{
+            System.out.println("Thank you for your time");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,42 @@
+
+# Challenges of the Java course in Coder House
+
+The purpose of this repository is to save and display the challenges that are presented within the Java course at Coder House.
+
+## Authors
+
+- [@pbasualdoarcati](https://www.github.com/pbasualdoarcati)
+
+
+## FAQ
+
+#### What course is it?
+
+Java programming course
+
+#### Who teaches it or on what platform is it being carried out?
+
+The platform is https://www.coderhouse.com/
+
+#### What topics are covered?
+- General concepts:
+    * Introduction to the Java world.
+    * Syntax and variables.
+    * Flow control.
+    * Loops/Iterations.
+    * Features.
+- Object Oriented Paradigm:
+    * Creation and manipulation of Classes and Objects, Interfaces and Inheritance of objects.
+- Database managment:
+    * Use of database.
+    * Data persistence in Java.
+- Spring boots:
+    * REST API.
+    * git.
+    * Consumption of external apis and validations.
+    * Aspects.
+
+#### Slogan of the first challenge
+Create a main method that allows you to display the integer part and separate the decimal part of a float data type.
+* Aspects to include:
+    * Deliver the class that executes without errors and leave the result in console.
